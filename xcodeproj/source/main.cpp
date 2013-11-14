5bfdf7dcd2c4272c8399a68685ed0de594a3288e
Akira Matsuda <akira.m.itachi@gmail.com>
2013-11-15 08:03:25 +0900

    optimize

--- xcodeproj/source/main.cpp
+++ xcodeproj/source/main.cpp
@@ -19,14 +19,15 @@ int main(int argc, const char * argv[])
 	// insert code here...
 	cout << "Hello, World!" << endl;
 
-	auto *cube = Object::cube("cube");
-	auto *pyramid = Object::pyramid3("pyramid");
+	auto cube = Object::cube("cube");
+	auto pyramid = Object::pyramid3("pyramid");
 	
 	cube->transform(Matrix::scale(10, 10, 10));
 	pyramid->transform(Matrix::scale(3, 3, 3));
+	
 	auto view = new xm3d(800, 800, "xm3d", [=](unsigned int life, XEvent e, Window window) {
-		cube->transform(Matrix::rotate(m3d_axis_y, 0.01));
-		pyramid->transform(Matrix::rotate(m3d_axis_y, 0.01));
+		cube->transform(Matrix::rotate(m3d_axis_y, 0.3));
+		pyramid->transform(Matrix::rotate(m3d_axis_y, 0.3));
 	});
 	view->add_obj(cube);
 	view->add_obj(pyramid);
