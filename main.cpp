1e7b9c115c94b17629726a05f9d4c1203e53b0e7
Akira Matsuda <akira.m.itachi@gmail.com>
2013-12-05 21:56:12 +0900

    change readme

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, const char * argv[])
 	Camera camera(Vector(10, 10, 10), Vector(0, 0, 0), Vector(0, 1, 0));
 	Light light(Vector(3, 4, 5), Vector(0, 0, 0), 1.0);
 	auto view = new xm3d(800, 800, camera, light, "xm3d", [=](unsigned int life, XEvent e, Window window) {
-//		polygon->transform(Matrix::rotate(m3d_axis_x, 1.0))->transform(Matrix::rotate(m3d_axis_y, 1.0))->transform(Matrix::rotate(m3d_axis_z, 1.0));
+		polygon->transform(Matrix::rotate(m3d_axis_x, 1.0))->transform(Matrix::rotate(m3d_axis_y, 1.0))->transform(Matrix::rotate(m3d_axis_z, 1.0));
 	});
 	view->add_object(*polygon);
 	view->run();
